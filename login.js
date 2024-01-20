9b18e84ef98b1853eb548823a759c2ff1b784159
OmarAstro <145227504+OmarAstro@users.noreply.github.com>
2024-01-20 12:22:41 +0200

    Update login.js

--- login.js
+++ login.js
@@ -4,6 +4,8 @@ function login() {
 
   if ((password === "26/5" && username === "toka")) {
     window.location.href = "login.html";
+  }else if ((password === "21" && username === "astro")) {
+    window.location.href = "login.html";
   } else {
     alert("كلمة المرور غير صحيحة");
   }
